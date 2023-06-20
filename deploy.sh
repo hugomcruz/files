b38f40bf3d59fbd80f91bf281d62562078f8dff5
Hugo Cruz <hugo.m.cruz@gmail.com>
2023-06-20 17:15:35 +0100

    New files

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+########
+#!/bin/sh
+
+
+### All files are in a Github repo:
+# https://github.com/hugomcruz/files 
+
+
+### MOCK INVENTORY
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/hugomcruz/files/main/k8s-mock-inventory.yaml
+
+### MOCK LOGBOOK
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/hugomcruz/files/main/k8s-mock-logbook.yaml
+
+
+### MOCK RESOLVE
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/hugomcruz/files/main/k8s-mock-resolve.yaml
+
